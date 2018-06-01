839d9f373153c212ceede16c647da4a34c4de386
radius2020 <kgs23@njit.edu>
2018-05-31 21:38:11 -0400

    review changes

--- service-worker.js
+++ service-worker.js
@@ -12,9 +12,20 @@ var cacheFiles = [
 	'./js/dbhelper.js',
 	'./js/restaurant_info.js',
 	'./js/app.js',
+	'./data/restaurants.json',
 	'./css/styles.css',
 	'./css/access_response_styles.css',
-	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic'
+	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg',
 ]
 
 self.addEventListener('install', function(e) {
@@ -43,6 +54,6 @@ self.addEventListener('activate', function(e) {
 	console.log("[Service Worker] activated");
 });
 self.addEventListener('fetch', function(e) {
-	console.log("[Service Worker] fetch:", e.request.url);
+	/*console.log("[Service Worker] fetch:", e.request.url);*/
 });
 
